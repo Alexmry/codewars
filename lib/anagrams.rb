c3d892fa57881eb2924a9a970c6363a08b4f23a8
Alexandre Morey <alexander006_9@hotmail.com>
2022-01-25 12:03:04 +0000

    anagrams set up

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,21 +1,27 @@
 def anagrams(word, words)
+    array_splitted_words = []
+    splitted_word = word.split('')
     #for each word in the array
     words.each do |w|
-
+       array_splitted_words << w.split("")
     end
 
-
-    # check if the letter of work are equivalent to the their letters
-
     # for each letter of each word, compare them with word
-
     # if the letter are the same, it does not have to be in the same order
     # => return the anagrams words
 
+    anagrams = []
+    array_splitted_words.each do |wo|
+      if splitted_word - wo = []
+        anagrams << wo
+      end
+    end
+
+    return anagrams
 
 end
 
-
+puts "#{anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada'])}"
 
 # anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
 
