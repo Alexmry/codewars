ff87b1b02f2f6e0ef92f68567f2a549b441051ed
Alexandre Morey <alexander006_9@hotmail.com>
2022-01-25 11:42:23 +0000

    anagrams set up

---
+++ lib/anagrams.rb
@@ -0,0 +1,23 @@
+def anagrams(word, words)
+    #for each word in the array
+    words.each do |w|
+
+    end
+
+
+    # check if the letter of work are equivalent to the their letters
+
+    # for each letter of each word, compare them with word
+
+    # if the letter are the same, it does not have to be in the same order
+    # => return the anagrams words
+
+
+end
+
+
+
+# anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+
+
+# [1, 2, 1].to_set == [2, 1, 1].to_set 
--- spec/replace_with_alphabet_position_spec.rb
+++ spec/replace_with_alphabet_position_spec.rb
@@ -1,7 +1,7 @@
 require "replace_with_alphabet_position"
 
 RSpec.describe ReplaceWithAlphabetPosition do
-    # subject(:alphabet_position) { described_class.alphabet_position }
+    subject(:alphabet_position) { described_class.alphabet_position }
 
     it "should pass fixed test" do
         # expect(alphabet_position).to eq("20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11")
@@ -9,7 +9,7 @@ RSpec.describe ReplaceWithAlphabetPosition do
         # @position_string.should_not be_nil 
         # expect(alphabet_position.length).to have_attributes(size: (be >= 0))
         # expect(alphabet_position).to receive(:foo).with(1, true)
-        expect(subject).to receive(:text)
+        expect(subject).to receive(:test).with(1)
     end
 
 end
