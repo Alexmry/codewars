a3e3dd741b416e4b3e88c9f2d8442176e6023973
Alexandre Morey <alexander006_9@hotmail.com>
2022-01-27 11:37:46 +0000

    set up

--- lib/test1_skyscanner.rb
+++ lib/test1_skyscanner.rb
@@ -1,3 +1,7 @@
+# write a fonction that takes a list
+# return the highest number of this list
+# if there are no number in the list return 0
+
 def solution(numbers)
     # Type your solution here
     sorted_number = numbers.sort
@@ -7,8 +11,6 @@ def solution(numbers)
     else
         sorted_number.last
     end
-    
-
 end
 
 puts "#{solution([7, 2, 6, 3])}"
