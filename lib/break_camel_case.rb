737dc4a6fba6c943b511f15b2987f5031db0b91b
Alexandre Morey <alexander006_9@hotmail.com>
2022-01-28 16:35:32 +0000

    setup

--- lib/break_camel_case.rb
+++ lib/break_camel_case.rb
@@ -7,22 +7,22 @@
 def solution(string)
   # complete the function
 
-  splited_string = string.split("")
-  downcase_letter = ""
+    splited_string = string.split("")
+    downcase_letter = ""
 
-  splited_string.select do |l|
-    if l == l.upcase
-        downcase_letter += " " + l
-    else
-        downcase_letter += l
-    end
+    splited_string.select do |l|
+        if l == l.upcase
+            downcase_letter += " " + l
+        else
+            downcase_letter += l
+        end
     # if l.downcase
     #      downcase_letter << l
     # end
     # return "#{l} and #{index}"
-  end
+    end
 
-return downcase_letter
+    return downcase_letter
 
 end
 
