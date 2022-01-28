a1f0746f528712704aa4c00528bd4fe38383eeaa
Alexandre Morey <alexander006_9@hotmail.com>
2022-01-28 16:29:10 +0000

    setup

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ https://github.com/adrianeyre/codewars/blob/master/Ruby/4KYU/Brace.rb
 
 
 https://gist.github.com/radiumrasheed/d144dc277f9f68de2a4a3def9be91982
+
+https://github.com/adrianeyre/codewars/blob/master/Ruby/5KYU/BreakCamel.rb
+
--- lib/break_camel_case.rb
+++ lib/break_camel_case.rb
@@ -10,14 +10,17 @@ def solution(string)
   splited_string = string.split("")
   downcase_letter = []
 
-  splited_string.each_with_index do |l, index|
-    if l.downcase
-         downcase_letter << l
+  splited_string.select do |l|
+    if l == l.upcase
+        downcase_letter << l
     end
+    # if l.downcase
+    #      downcase_letter << l
+    # end
     # return "#{l} and #{index}"
   end
 
-  return downcase_letter
+    return downcase_letter
 end
 
 puts "#{solution("camelCasing")}"
