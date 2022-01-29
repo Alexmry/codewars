b8d6fd566280f6833eb08a3cd28f825bc684c4ec
Alexandre Morey <alexander006_9@hotmail.com>
2022-01-29 11:42:50 +0000

    done

--- lib/number_of_trailing_zeros_of_n.rb
+++ lib/number_of_trailing_zeros_of_n.rb
@@ -1,5 +1,12 @@
 def zeros(n)
 
-    factorial_of_n = (1..n).to_a.inject(:*)
+    # factorial_of_n = (1..n).to_a.inject(:*)
+
+    i, ans = 5, 0
+    while n/i > 0
+        ans += n/i
+        i *= 5
+    end
+    ans
   
 end
