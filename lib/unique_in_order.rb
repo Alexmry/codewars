f9802a9bb17fdc0fb7f69912264b39f5d3b58b74
Alexandre Morey <alexander006_9@hotmail.com>
2022-01-31 14:45:01 +0000

    done

--- lib/unique_in_order.rb
+++ lib/unique_in_order.rb
@@ -1,15 +1,20 @@
 def unique_in_order(iterable)
   #your code here
 
-  arr = iterable.split("")
-  array = arr.each_slice(2).to_a
-  results = []
-  array.each do |a|
-    b = a.uniq
-    results << b
-  end
+  # arr = iterable.split("")
+  # array = arr.each_slice(2).to_a
+  # results = []
+  # array.each do |a|
+  #   b = a.uniq
+  #   results << b
+  # end
   
-  return results
+  # return results
+
+  result = []
+  iterable = iterable.split("") if !iterable.is_a? Array
+  iterable.each_with_index {|x,i| result << x if iterable[i-1] != x || i == 0}
+  result
 
 end
 
