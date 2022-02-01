e9f12a93378201cdb994247feb984a89bfd989d7
Alexandre Morey <alexander006_9@hotmail.com>
2022-02-01 13:53:55 +0000

    yes

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,22 +1,36 @@
 def permutations(string)
   #your code here
-#   string_shuffled = string.split("").shuffle.join
-  array_of_shuffled_string = []
-
-  x = 0
+#   array_of_shuffled_string = []
+  
+    # string_shuffled = string.split("").shuffle.join
+#   x = 0
 
 #   while array_of_shuffled_string.include?(string_shuffled)
-
+#     array_of_shuffled_string << string_shuffled
+#     string_shuffled = string.split("").shuffle.join
 #   end
 
-    while x <= 1000
-        array_of_shuffled_string << string.split("").shuffle.join
-        x += 1
+    # while x <= 1000
+    #     array_of_shuffled_string << string.split("").shuffle.join
+    #     x += 1
+    # end
+    # return array_of_shuffled_string.uniq
+
+    letters = string.split('')
+    # letters.combination(2).to_a
+    big_array = letters.permutation(letters.length).to_a
+
+    array_of_strings = []
+
+    big_array.each do |array|
+        stringgg = array.join("")
+        array_of_strings << stringgg
     end
 
-    return array_of_shuffled_string.uniq
+    return array_of_strings.uniq
 
 end
 
 
 puts "#{permutations('aabb')}"
+# ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
