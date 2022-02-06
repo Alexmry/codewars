ada525ab80122cb844be5be298c2bcbb56028b91
Alexandre Morey <alexander006_9@hotmail.com>
2022-02-06 19:09:18 +0000

    done

--- lib/simple_pig_latin.rb
+++ lib/simple_pig_latin.rb
@@ -1,12 +1,23 @@
 def pig_it(text)
     result = []
+
+    last = []
+
+    if text.split(" ").last == ["!"]
+        last << text.split(" ").last
+        text.pop
+    end
+
     text.split(" ").each do |word|
         new_word = word.split("").drop(1) << word.split("").first << "ay"
         result << new_word.join("")
     end
 
-    result.join(" ")
+    res = result << last
+    res.join(" ")
 end
 
 
 puts "#{pig_it('Pig latin is cool')}"  # igPay atinlay siay oolcay
+
+puts "#{pig_it('Hello world !')}"   # elloHay orldway !
