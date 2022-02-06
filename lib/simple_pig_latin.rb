3ec5930134a44db7c51e7123b12aacd0b88584e0
Alexandre Morey <alexander006_9@hotmail.com>
2022-02-06 17:13:06 +0000

    done

---
+++ lib/simple_pig_latin.rb
@@ -0,0 +1,12 @@
+def pig_it(text)
+    result = []
+    text.split(" ").each do |word|
+        new_word = word..split("").drop(1) << "ay"
+        result << new_word.join("")
+    end
+
+    result.join("")
+end
+
+
+puts "#{pig_it('Pig latin is cool')}"  # igPay atinlay siay oolcay
