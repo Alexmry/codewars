24cf44aa75c3dc7cfd916b72816b7df47f2301f9
Alexandre Morey <alexander006_9@hotmail.com>
2022-02-13 16:20:48 +0000

    done

--- lib/pete_the_baker.rb
+++ lib/pete_the_baker.rb
@@ -1,16 +1,21 @@
 def cakes(recipe, available)
   #your code here
-
+  item_available = []
   # for each element if the recipe hash
+  recipe.each do |ke, value|
+    if available.key?(ke)
+       item_available << value
+    end
+  end
   # check if key? is true in available hash?
   # if it's true, compare the quantity
   # for the ingredient with the loweest quantity => return modulo
 
-  
+  return item_available
 end
 
 
 # must return 2
-# cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}); 
+puts "#{cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200})}"; 
 # must return 0
 # cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000});
